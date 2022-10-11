982d4e02fb5dabe3b6e4046eb35986f0d97804d9
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-10-11 16:09:42 +0200

    User getRole and added in table

--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,20 +1,22 @@
 <div>
-    <table class="table-auto">
-        <thead>
+    <table class="table-auto mb-4 w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th>Id</th>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Role</th>
+                <th scope="col" class="py-3 px-6">Id</th>
+                <th scope="col" class="py-3 px-6">Username</th>
+                <th scope="col" class="py-3 px-6">Email</th>
+                <th scope="col" class="py-3 px-6">Role</th>
+                <th scope="col" class="py-3 px-6">Edit</th>
             </tr>
         </thead>
         <tbody>
             @foreach ($users as $user)
-                <tr>
-                    <td>{{$user->id}}</td>
-                    <td>{{$user->username}}</td>
-                    <td>{{$user->email}}</td>
-                    <td>{{$user->role_id}}</td>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <td scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">{{$user->id}}</td>
+                    <td class="py-4 px-6">{{$user->username}}</td>
+                    <td class="py-4 px-6">{{$user->email}}</td>
+                    <td class="py-4 px-6">{{$user->getRole->name}}</td>
+                    <td class="py-4 px-6"><a href="#">Edit</a></td>
                 </tr>
             @endforeach
         </tbody>

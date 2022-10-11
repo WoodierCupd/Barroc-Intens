57c83f55d08a0649285ac786e9eb8fbe89fc3b82
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-10-11 10:50:03 +0200

    renamed role to roles

--- database/migrations/2022_10_11_084841_create_roles_table.php
+++ database/migrations/2022_10_11_084841_create_roles_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('role', function (Blueprint $table) {
+        Schema::create('roles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->timestamps();
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('role');
+        Schema::dropIfExists('roles');
     }
 };
--- database/migrations/2022_10_11_084926_add_role_to_users_table.php
+++ database/migrations/2022_10_11_084926_add_role_to_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->unsignedBigInteger('role_id');
-            $table->foreign('role_id')->references('id')->on('role')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
     }
 

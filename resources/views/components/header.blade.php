c67aae26d0c810d3c6ee66097b548e41ae7d6505
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-10-11 13:46:22 +0200

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,14 +4,19 @@
             <div class="flex justify-start basis-1/6">
                 <a class="" href="{{route('home')}}"><img class="w-20" src="{{ asset('img/Logo6_groot.png') }}" alt="Barroc intens logo"></a>
             </div>
-            <div class="flex justify-end items-center basis-5/6">
+            <div class="flex justify-center items-center basis-2/3">
+                <a href="#" class="text-lg px-2 text-gray-700 dark:text-gray-500 underline">Link 1</a>
+                <a href="#" class="text-lg px-2 text-gray-700 dark:text-gray-500 underline">Link 2</a>
+                <a href="#" class="text-lg px-2 text-gray-700 dark:text-gray-500 underline">Link 3</a>
+            </div>
+            <div class="flex justify-end items-center basis-1/6">
                 @auth
-                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    <a href="{{ url('/dashboard') }}" class="text-lg text-gray-700 dark:text-gray-500 underline">Dashboard</a>
                 @else
-                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                    <a href="{{ route('login') }}" class="text-lg text-gray-700 dark:text-gray-500 underline">Log in</a>
 
                     @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                        <a href="{{ route('register') }}" class="ml-4 text-lg text-gray-700 dark:text-gray-500 underline">Register</a>
                     @endif
                 @endauth
             </div>

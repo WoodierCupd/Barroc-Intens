8cdc6fb3d408483d018098d461d066833237dc44
basvandenmaagdenberg <71122658+BasvandenMaagdenberg@users.noreply.github.com>
2022-10-11 11:06:25 +0200

    added image and content

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,16 @@
 <x-head/>
-<body>
-    <x-header></x-header>
+<body class="bg-black">
 
-    {{--TODO: hier komt de homepage--}}
+    <main>
+        <div class="hero px-2 py-64 font-bold">
+            <h1 class="text-6xl text-white">Kwaliteit en Smaak</h1>
+            <p class=" py-10 text-white">Baroc Intens, bizar lekkere koffie! En goedkoop!</p>
+            <div class="hero-buttons">
+                <button class="bg-yellow-400 hover:bg-yellow-500 text-white py-2 px-4 mx-2 rounded" >BESTEL KOFFIE</button>
+                <button class="bg-yellow-400 hover:bg-yellow-500 text-white py-2 px-4 mx-2 rounded" >HUUR MACHINE</button>
+            </div>
+        </div>
+    </main>
 
     <x-footer/>
     @livewireScripts

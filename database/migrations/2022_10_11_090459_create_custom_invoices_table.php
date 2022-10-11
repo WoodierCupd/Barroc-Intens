ad4f6bf4565ca6a5935cae5c8003570a14c36495
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-10-11 11:07:12 +0200

    Create 2022_10_11_090459_create_custom_invoices_table.php

---
+++ database/migrations/2022_10_11_090459_create_custom_invoices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('custom_invoices', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('date');
+            $table->dateTime('paid_at')->nullable();
+            $table->unsignedBigInteger('company_id');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('custom_invoices');
+    }
+};

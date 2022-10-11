de405712d034f19e9c4f5adef993649569d4e313
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-10-11 14:15:18 +0200

    Made admin page

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,8 +15,11 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                    <x-nav-link :href="route('home')" :active="request()->routeIs('dashboard')">
+                    <x-nav-link :href="route('home')">
                         {{ __('Home') }}
+                    </x-nav-link>
+                    <x-nav-link :href="route('admin')" :active="request()->routeIs('admin')">
+                        {{ __('Admin') }}
                     </x-nav-link>
                 </div>
             </div>

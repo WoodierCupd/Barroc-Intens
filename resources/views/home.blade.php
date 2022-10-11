60cc66503dd4f73a43fb75d14177724172a47ffc
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-10-11 11:44:21 +0200

    removed main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,20 @@
 <x-head/>
 <body class="bg-white">
+    <x-header/>
 
-    <main>
-        <div class="hero px-2 py-64 font-bold">
-            <h1 class="text-6xl text-white">Kwaliteit en Smaak</h1>
-            <p class=" py-10 text-white">Baroc Intens, bizar lekkere koffie! En goedkoop!</p>
-            <div class="hero-buttons">
-                <button class="heroButton hover:bg-yellow-500 text-white py-2 px-4 mx-2 rounded" >BESTEL KOFFIE</button>
-                <button class="heroButton hover:bg-yellow-500 text-white py-2 px-4 mx-2 rounded" >HUUR MACHINE</button>
+        <div class="hero_bg">
+            <div class="hero px-2 py-64 font-bold">
+                <h1 class="text-6xl text-white">Kwaliteit en Smaak</h1>
+                <p class=" py-10 text-white">Baroc Intens, bizar lekkere koffie! En goedkoop!</p>
+                <div class="hero-buttons">
+                    <button class="heroButton hover:bg-yellow-500 text-white py-2 px-4 mx-2 rounded" >BESTEL KOFFIE</button>
+                    <button class="heroButton hover:bg-yellow-500 text-white py-2 px-4 mx-2 rounded" >HUUR MACHINE</button>
+                </div>
             </div>
         </div>
 
 
-    </main>
-        <x-grid/>
+    <x-grid/>
     <x-footer/>
     @livewireScripts
 </body>

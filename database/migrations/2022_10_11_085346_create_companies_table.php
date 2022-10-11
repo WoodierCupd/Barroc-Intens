d1db77b0c1c9da0e44273095c9194ab1745a2510
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-10-11 10:54:07 +0200

    Create 2022_10_11_085346_create_companies_table.php

---
+++ database/migrations/2022_10_11_085346_create_companies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->string('street');
+            $table->string('house_number');
+            $table->string('city');
+            $table->string('country_code');
+            $table->dateTime('bkr_checked')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+};

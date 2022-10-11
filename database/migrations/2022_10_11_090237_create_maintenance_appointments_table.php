fc531d30c99320cc9f08106b6cb65750702d7b12
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-10-11 11:04:28 +0200

    Create 2022_10_11_090237_create_maintenance_appointments_table.php

---
+++ database/migrations/2022_10_11_090237_create_maintenance_appointments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('maintenance_appointments', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('company_id');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->longText('remark');
+            $table->dateTime('date_added');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('maintenance_appointments');
+    }
+};

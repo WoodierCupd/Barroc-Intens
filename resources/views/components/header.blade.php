20f6f75c03597fe7405fc7b08da80c4c6d60a277
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-10-11 13:35:47 +0200

    Update header.blade.php

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,18 +1,20 @@
-<div class="relative flex items-top justify-center bg-yellow-500  py-4 sm:pt-0 ">
+<div class="sticky top-0 z-50 bg-barroc_yellow">
     @if (Route::has('login'))
-        <div class="flex top-0 right-0 justify-between px-6 py-4">
-            <div class="">
-                <a href="{{route('home')}}"><img src="{{ asset('img/Logo6_groot.png') }}" alt="Barroc intens logo"></a>
+        <div class="relative flex flex-row px-4 py-2">
+            <div class="flex justify-start basis-1/6">
+                <a class="" href="{{route('home')}}"><img class="w-20" src="{{ asset('img/Logo6_groot.png') }}" alt="Barroc intens logo"></a>
             </div>
-            @auth
-                <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-            @else
-                <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+            <div class="flex justify-end items-center basis-5/6">
+                @auth
+                    <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
 
-                @if (Route::has('register'))
-                    <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
-                @endif
-            @endauth
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
+                    @endif
+                @endauth
+            </div>
         </div>
     @endif
 </div>

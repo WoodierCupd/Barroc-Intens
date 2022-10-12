459d70927e3d3815f4319ec375cc53a690398c71
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-10-12 11:02:34 +0200

    From works (Without password and validation) :lemon:

--- app/Http/Livewire/UserEdit.php
+++ app/Http/Livewire/UserEdit.php
@@ -13,12 +13,22 @@ class UserEdit extends Component
     public $username;
     public $password;
     public $email;
+    public $role;
 
     public function mount(){
         $this->roles = Role::all();
         $this->name = $this->user->name;
         $this->username = $this->user->username;
         $this->email = $this->user->email;
+        $this->role = $this->user->role_id;
+    }
+
+    public function submit(){
+        $this->user->name = $this->name;
+        $this->user->username = $this->username;
+        $this->user->email = $this->email;
+        $this->user->role_id = $this->role;
+        $this->user->save();
     }
 
     public function render()
--- resources/views/livewire/user-edit.blade.php
+++ resources/views/livewire/user-edit.blade.php
@@ -24,12 +24,12 @@
             </div>
         </div>
         <label for="role" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Role</label>
-        <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        <select wire:model="role" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
             @foreach($roles as $curRole)
-                <option value="{{$curRole->id}}" @if($curRole->id == $user->role_id) selected @endif>{{$curRole->name}}</option>
+                <option value="{{$curRole->id}}">{{$curRole->name}}</option>
             @endforeach
         </select>
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Confirm edit</button>
+        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 my-2.5">Confirm edit</button>
     </form>
     <script>
         function showPassword() {

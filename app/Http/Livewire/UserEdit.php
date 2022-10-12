cd0b7d15190f218fbd0f902c91f394a858593bbb
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-10-12 10:44:24 +0200

    Form styling done!

--- app/Http/Livewire/UserEdit.php
+++ app/Http/Livewire/UserEdit.php
@@ -2,18 +2,27 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Role;
 use Livewire\Component;
 
 class UserEdit extends Component
 {
     public $user;
+    public $roles;
     public $name;
     public $username;
+    public $password;
+    public $email;
+
+    public function mount(){
+        $this->roles = Role::all();
+        $this->name = $this->user->name;
+        $this->username = $this->user->username;
+        $this->email = $this->user->email;
+    }
 
     public function render()
     {
-        $this->name = $this->user->name;
-        $this->username = $this->user->username;
         return view('livewire.user-edit');
     }
 }

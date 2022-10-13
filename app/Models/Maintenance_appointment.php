7be9a3e9a40a6c605caa5a9c597bef16b919953e
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-10-13 15:22:58 +0200

    maintenance_appointment model

---
+++ app/Models/Maintenance_appointment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Maintenance_appointment extends Model
+{
+    use HasFactory;
+    protected $table = 'maintenance_appointment';
+    protected $fillable = [
+        'company_id',
+        'remark',
+        'date_added',
+    ];
+
+    public function getCompany()
+    {
+        return $this->belongsTo(Company::class, 'company_id', 'id');
+
+    }
+}

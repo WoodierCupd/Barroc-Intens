619de06e7524d5c9bc0c820b2a8d28ac6e3cc44b
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-10-17 13:35:30 +0200

    Product edit works now :snake:

---
+++ app/Http/Livewire/ProductEdit.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Product;
+use App\Models\Product_category;
+use Livewire\Component;
+
+class ProductEdit extends Component
+{
+    public $name;
+    public $price;
+    public $category;
+    public $description;
+    public $categories;
+    public $product;
+
+    public function mount(){
+        $this->categories = Product_category::all();
+        $this->name = $this->product->name;
+        $this->price = $this->product->price;
+        $this->category = $this->product->category;
+        $this->description = $this->product->description;
+        $this->category = $this->product->product_category_id;
+    }
+
+    public function submit(){
+        $this->product->name = $this->name;
+        $this->product->price = $this->price;
+        $this->product->product_category_id = $this->category;
+        $this->product->description = $this->description;
+        $this->product->save();
+        session()->flash('message', 'Product successfully updated.');
+    }
+
+    public function render()
+    {
+        return view('livewire.product-edit');
+    }
+}
---
+++ resources/views/livewire/product-edit.blade.php
@@ -0,0 +1,38 @@
+<div class="pt-6">
+    <form wire:submit.prevent="submit">
+        <div class="grid md:grid-cols-2 md:gap-6">
+            <div class="relative z-0 mb-6 w-full group">
+                <input type="text" wire:model="name" name="name" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"/>
+                <label for="name" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Name</label>
+            </div>
+            <div class="relative z-0 mb-6 w-full group">
+                <input type="text" wire:model="description" name="description" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"/>
+                <label for="description" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Description</label>
+            </div>
+        </div>
+        <div class="grid md:grid-cols-2 md:gap-6">
+            <div class="relative z-0 mb-6 w-full group">
+                <input type="number" step="0.01" wire:model="price" name="price" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"/>
+                <label for="price" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Price</label>
+            </div>
+            <div class="relative z-0 mb-6 w-full group">
+                <label for="user" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Category</label>
+                <select wire:model="category" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                    @foreach($categories as $category)
+                        <option value="{{$category->id}}">{{$category->name}}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        <div class="grid md:grid-cols-2 md:gap-6">
+            <div class="relative z-0 w-full group">
+                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create product</button>
+            </div>
+        </div>
+    </form>
+    @if (session()->has('message'))
+        <div class="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3 mt-3" role="alert">
+            <p>{{ session('message') }}</p>
+        </div>
+    @endif
+</div>

ff4ed0cfd5c821ae03e6421a7617c34f11155826
Floris Bogers <71845444+FlorisBo@users.noreply.github.com>
2022-10-21 10:20:21 +0200

    temp

--- resources/views/product-index.blade.php
+++ resources/views/product-index.blade.php
@@ -1,1 +1,21 @@
-{{$product}}
+<x-head/>
+<x-header/>
+<body>
+    <div class="container">
+        <div class="section">
+            <div class="product__grid">
+                <div class="product__title">
+                    <h2>{{ $product->name }}</h2>
+                </div>
+                <div class="product__image">
+                    <img src="{{ $product->image_path }}" alt="">
+                </div>
+                <div class="product__">
+                    <p>{{ $product->description }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+<x-footer/>
+@livewireScripts

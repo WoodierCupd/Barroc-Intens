2595af4b94fe400463a5a19ed01408a4b315d429
Floris Bogers <71845444+FlorisBo@users.noreply.github.com>
2022-10-31 13:52:19 +0100

    product details page
    * rows zijn niet goed gestyled

--- resources/views/product-index.blade.php
+++ resources/views/product-index.blade.php
@@ -10,8 +10,9 @@
                 <div class="product__image">
                     <img src="{{ $product->image_path }}" alt="">
                 </div>
-                <div class="product__">
+                <div class="product__description">
                     <p>{{ $product->description }}</p>
+                    <a href="{{route('contactform')}}" class="product__btn">Vraag een offerte aan!</a>
                 </div>
             </div>
         </div>

d08e729d0e3eb0789af1adf7bb88ae293d2e860a
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-11-02 11:43:24 +0100

    Update product-index.blade.php

--- resources/views/product-index.blade.php
+++ resources/views/product-index.blade.php
@@ -3,16 +3,20 @@
 <body>
     <div class="container">
         <div class="section">
+            <div class="product__name">
+                <h2>{{ $product->name }}</h2>
+            </div>
             <div class="product__grid">
-                <div class="product__name">
-                    <h2>{{ $product->name }}</h2>
-                </div>
                 <div class="product__image">
                     <img src="{{ $product->image_path }}" alt="">
                 </div>
                 <div class="product__description">
                     <p>{{ $product->description }}</p>
-                    <a href="mailto:sales@barrocintens.nl?subject={{ str_replace(" ", "%20", $product->name) }}" class="product__btn">Vraag een offerte aan!</a>
+                    @auth()
+                        <a href="mailto:sales@barrocintens.nl?subject={{ str_replace(" ", "%20", $product->name) }}" class="product__btn">Vraag een offerte aan!</a>
+                    @else
+                        <p class="italic text-red-600">Je kan alleen producten kopen als je een account aanvraagt en de BKR check haalt.</p>
+                    @endauth
                 </div>
             </div>
         </div>

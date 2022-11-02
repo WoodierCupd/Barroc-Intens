447595c2301bd6e4382c0456647a67c2220e4515
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-11-02 10:18:41 +0100

    Created livewire stuff

---
+++ app/Http/Livewire/NoteCreate.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class NoteCreate extends Component
+{
+    public function render()
+    {
+        return view('livewire.note-create');
+    }
+}

7f3ccd3800ac2f8f2a8b99656e67b1cbedc965b9
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-11-03 14:05:10 +0100

    Update offerte aanvragen

--- app/Http/Livewire/QuotationRequest.php
+++ app/Http/Livewire/QuotationRequest.php
@@ -2,11 +2,25 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Company;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class QuotationRequest extends Component
 {
     public $product;
+
+    public function request(){
+        if (Auth::user()->role_id != 2){
+            session()->flash('message', 'Alleen een klant kan een offerte aanvragen. Personeel niet!');
+        } elseif (!Company::firstWhere('contact_id', 2)){
+            session()->flash('message', 'Je moet eerst een company aanmaken. Neem contact op met support.');
+        } elseif (!Company::firstWhere('contact_id', 2)->bkr_checked){
+            session()->flash('message', 'Je moet eerst de BKR check halen. Neem contact op met support.');
+        } else {
+            dd('je kan het aanvragen');
+        }
+    }
 
     public function render()
     {
--- resources/views/livewire/quotation-request.blade.php
+++ resources/views/livewire/quotation-request.blade.php
@@ -5,16 +5,21 @@
         </div>
         <div class="product__grid">
             <div class="product__image">
-                <img src="{{ $product->image_path }}" alt="">
+                <img src="{{ $product->image_path }}" alt="Product image">
             </div>
             <div class="product__description">
                 <p>{{ $product->description }}</p>
                 @auth()
-                    <button class="block bg-mettalic-yellow max-w-fit font-bold px-5 py-2 rounded-sm mt-2 shadow-lg">Vraag een offerte aan</button>
+                    <button wire:click="request" class="block bg-mettalic-yellow max-w-fit font-bold px-5 py-2 rounded-sm mt-2 shadow-lg">Vraag een offerte aan</button>
                 @else
                     <p class="italic text-red-600">Je kan alleen producten huren als je een account aanvraagt en de BKR check haalt.</p>
                 @endauth
             </div>
         </div>
     </div>
+    @if (session()->has('message'))
+        <div class="bg-red-100 border-t border-b border-red-500 text-red-700 px-4 py-3 mt-3" role="alert">
+            <p>{{ session('message') }}</p>
+        </div>
+    @endif
 </div>

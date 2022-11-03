1c753789b349a35a524a3e7d0c9667cf0e7822f0
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-11-03 13:16:16 +0100

    Made descriptions from products more than 255 chars max

--- database/migrations/2022_10_11_090805_create_products_table.php
+++ database/migrations/2022_10_11_090805_create_products_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->string('image_path')->nullable();
             $table->decimal('price');
             $table->timestamps();

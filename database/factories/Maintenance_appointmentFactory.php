9bd1a0a1852b22073b210afab8c4e7d6e65d876c
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-11-04 09:48:42 +0100

    Maintanance appointment type

--- database/factories/Maintenance_appointmentFactory.php
+++ database/factories/Maintenance_appointmentFactory.php
@@ -22,6 +22,7 @@ class Maintenance_appointmentFactory extends Factory
         return [
             'remark' => fake()->sentence(rand(150, 300)),
             'date_added' => fake()->dateTimeBetween('+2 days', '+60 days'),
+            'type' => fake()->numberBetween(0, 1)
         ];
     }
 }

9cc122b2296a0460d79fceed372c36eeb94872d9
Floris Bogers <71845444+FlorisBo@users.noreply.github.com>
2022-11-08 11:01:50 +0100

    bug fix

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,18 +1,18 @@
-<div class="sticky top-0 z-50 bg-mettalic-yellow">
+<div class="sticky top-0 z-50 bg-white">
     @if (Route::has('login'))
         <div class="relative flex flex-row px-4 py-2">
             <div class="flex justify-start basis-1/6">
-                <a class="" href="{{route('home')}}"><img class="w-28" src="{{ asset('img/Logo6_groot.png') }}" alt="Barroc intens logo"></a>
+                <a class="bg-mettalic-yellow" href="{{route('home')}}"><img class="w-28" src="{{ asset('img/Logo6_groot.png') }}" alt="Barroc intens logo"></a>
             </div>
-            <div class=" flex justify-center items-center basis-2/3">
+            <div class="flex justify-center items-center basis-2/3">
                 <a href="#" class="text-lg px-2 text-white"><button class="bg-black rounded py-2 px-2 hover:text-black hover:bg-yellow-500">Link 1</button> </a>
-                <a href="/products" class="text-lg px-2 text-white"><button class="bg-black rounded py-2 px-2 hover:text-black hover:bg-yellow-500">Producten</button> </a>
+                <a href="{{ route('products') }}" class="text-lg px-2 text-white"><button class="bg-black rounded py-2 px-2 hover:text-black hover:bg-yellow-500">Producten</button> </a>
                 <a href="#" class="text-lg px-2 text-white"><button class="bg-black rounded py-2 px-2 hover:text-black hover:bg-yellow-500">Link 3</button> </a>
             </div>
             <div class="flex justify-end items-center basis-1/6">
-                    <a href="{{route('contactform')}}" class="text-xl px-2">Contact</a>
+                    <a href="{{ route('contactform') }}" class="text-xl px-2">Contact</a>
                 @auth
-                    <a href="{{ url('/dashboard') }}" class="text-xl px-2">Dashboard</a>
+                    <a href="{{ route('dashboard') }}" class="text-xl px-2">Dashboard</a>
                 @else
                     <a href="{{ route('login') }}" class="text-xl">Log in</a>
                 @endauth
--- resources/views/contactform.blade.php
+++ resources/views/contactform.blade.php
@@ -1,6 +1,4 @@
 <x-head/>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-      integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
 <body class="bg-white">
 

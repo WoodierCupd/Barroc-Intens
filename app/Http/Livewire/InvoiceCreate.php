13f9e1217f21bd169e6e14b18025239245643672
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-11-08 13:12:13 +0100

    Form styling done

--- app/Http/Livewire/InvoiceCreate.php
+++ app/Http/Livewire/InvoiceCreate.php
@@ -2,10 +2,22 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Company;
 use Livewire\Component;
 
 class InvoiceCreate extends Component
 {
+    public $companies;
+    public $paid_at;
+
+    public function mount(){
+        $this->companies = Company::all();
+    }
+
+    public function submit(){
+        dd('Ok');
+    }
+
     public function render()
     {
         return view('livewire.invoice-create');

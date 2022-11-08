61487c2c6ca9cad94c8ad983272971020e437756
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-11-08 11:38:31 +0100

    Create CustomInvoiceFactory.php

---
+++ database/factories/CustomInvoiceFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CustomInvoice>
+ */
+class CustomInvoiceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'date' => Carbon::now();
+        ];
+    }
+}

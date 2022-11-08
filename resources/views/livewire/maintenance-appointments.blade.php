1d200373c7b9efa0adf7949bbeabd1b3964cfe4e
Torben <D284117@edu.rocwb.nl>
2022-11-08 11:12:23 +0100

    header

--- resources/views/livewire/maintenance-appointments.blade.php
+++ resources/views/livewire/maintenance-appointments.blade.php
@@ -3,10 +3,11 @@
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
         <tr>
             <th scope="col" class="py-3 px-6">Id</th>
-            <th scope="col" class="py-3 px-6">Creator</th>
-            <th scope="col" class="py-3 px-6">Date</th>
+            <th scope="col" class="py-3 px-6">Maker</th>
+            <th scope="col" class="py-3 px-6">Datum</th>
             <th scope="col" class="py-3 px-6">Type</th>
-            <th scope="col" class="py-3 px-6">Detail page</th>
+            <th scope="col" class="py-3 px-6">Detail pagina</th>
+            <th scope="col" class="py-3 px-6">Verantwoordelijke</th>
         </tr>
         </thead>
         <tbody>

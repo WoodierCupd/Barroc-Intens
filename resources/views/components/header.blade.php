ea8f0ebc3d7b6a82cbe564cc1003c47fde538de5
Floris Bogers <71845444+FlorisBo@users.noreply.github.com>
2022-11-09 10:33:15 +0100

    New Header

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,23 +1,20 @@
-<div class="sticky top-0 z-50 bg-white" style="box-shadow: 0 -6px 10px 5px rgba(0,0,0,0.5);">
+<header>
     @if (Route::has('login'))
-        <div class="relative flex flex-row px-4 py-2">
-            <div class="flex justify-start basis-1/6">
-                <a  href="{{route('home')}}"><img class="w-28" src="{{ asset('img/Logo6_groot.png') }}" alt="Barroc intens logo"></a>
+        <div class="navbar">
+            <div class="navbar__logo">
+                <a  href="{{route('home')}}"><img src="{{ asset('img/Logo6_groot.png') }}" alt="Barroc intens logo"></a>
             </div>
-            <div class="flex justify-center items-center basis-2/3">
-                <a href="#" class="text-lg px-2 text-white"><button class="bg-black rounded py-2 px-2 hover:text-black hover:bg-yellow-500">Link 1</button> </a>
-                <a href="{{ route('products') }}" class="bg-barroc_yellow text-barroc_grey hover:text-barroc_yellow hover:bg-barroc_grey py-2 px-4 mx-2 rounded transition ease-in-out duration-150">Producten</a>
-                <a href="#" class="text-lg px-2 text-white"><button class="bg-black rounded py-2 px-2 hover:text-black hover:bg-yellow-500">Link 3</button> </a>
+            <div class="navbar__items">
+                <a href="{{ route('products') }}" class="navbar__btn">Producten</a>
+                <a href="{{ route('contactform') }}" class="navbar__btn">Contact</a>
             </div>
-            <div class="flex justify-end items-center basis-1/6">
-                    <a href="{{ route('contactform') }}" class="text-xl px-2">Contact</a>
+            <div class="navbar__cta">
                 @auth
-                    <a href="{{ route('dashboard') }}" class="text-xl px-2">Dashboard</a>
+                    <a href="{{ route('dashboard') }}" class="navbar__btn--cta">Dashboard</a>
                 @else
-                    <a href="{{ route('login') }}" class="text-xl">Log in</a>
-                    <a href="{{ route('login') }}" class="text-lg px-2 text-white bg-black rounded py-2 px-2 hover:text-black hover:bg-yellow-500">Log in</a>
+                    <a href="{{ route('login') }}" class="navbar__btn--cta">Log in</a>
                 @endauth
             </div>
         </div>
     @endif
-</div>
+</header>

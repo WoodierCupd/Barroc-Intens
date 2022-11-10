bb4724fbc8e1955898e24982cfaa8b8e8f584866
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-11-10 15:06:32 +0100

    added stock to products

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -24,6 +24,7 @@ class ProductFactory extends Factory
             'name' => fake()->word(),
             'description' => fake()->text(rand(1000, 2000)),
             'price' => fake()->randomFloat(2, 5, 200),
+            'stock' => fake()->randomFloat(0, 0, 10),
             'image_path' => fake()->imageUrl(),
         ];
     }

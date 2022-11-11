4c907050d04f3e601788c1311f13585946383660
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-11-11 09:16:09 +0100

    Removed

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -25,7 +25,7 @@ class ProductFactory extends Factory
             'description' => fake()->text(rand(1000, 2000)),
             'price' => fake()->randomFloat(2, 5, 200),
             'stock' => fake()->randomFloat(0, 0, 10),
-            'image_path' => (rand(0, 1) == 1) ? 'https://filerepository.itslearning.com/e111f4bb-8a86-4487-99ac-66cc9a577a3e?Token=jbcGACMDAAARAm5jAAAAACAAMW91IKDHGipWeLUZJRi9aX9zpegDTCgm5LfVjx86SDcAAA' : 'https://filerepository.itslearning.com/beb82afd-3398-4a41-8199-509bfdfb1094?Token=jbcGACMDAAA0Am5jAAAAACAAEjDlOtHSVaT7Jpbv2kqKFeAOU-R0-IOKl1Zm7FlY3S8AAA',
+            'image_path' => fake()->imageUrl(),
         ];
     }
 }

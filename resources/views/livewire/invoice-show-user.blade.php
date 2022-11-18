3b8ba9be05b84cb487f9103861d5f658de5b0ca2
WoodierCupd <74969069+WoodierCupd@users.noreply.github.com>
2022-11-18 09:49:29 +0100

    Update invoice-show-user.blade.php

--- resources/views/livewire/invoice-show-user.blade.php
+++ resources/views/livewire/invoice-show-user.blade.php
@@ -1,5 +1,5 @@
 <div>
-    @if(count($invoices))
+    @if($invoices !== null)
         <table class="table-auto mb-4 w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
